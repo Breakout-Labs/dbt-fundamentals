fb6385a180934b934f2708c2aa6d3681ee1edf94
Kevin Campbell <kevin-campbell@siemens.com>
2025-08-26 15:54:09 +0000

    Lab 2 completed

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -15,6 +15,12 @@ customers as (
     from {{ ref('stg_ecomm__customers') }}
 ),
 
+customer_survey as (
+    select
+    *
+    from {{ ref('stg_sheets__customer_survey_responses') }}
+),
+
 customer_metrics as (
     select
         customer_id,
@@ -22,7 +28,10 @@ customer_metrics as (
         min(ordered_at) as first_order_at,
         max(ordered_at) as most_recent_order_at,
         avg(delivery_time_from_collection) as average_delivery_time_from_collection,
-        avg(delivery_time_from_order) as average_delivery_time_from_order
+        avg(delivery_time_from_order) as average_delivery_time_from_order,
+        {% for days in [30,90,360] %}
+        count(case when ordered_at > current_date() - {{days}} then 1 end) as count_orders_last_{{days}}_days,
+        {%endfor%}
     from orders
     group by 1
 
@@ -35,10 +44,18 @@ joined as (
         customer_metrics.first_order_at,
         customer_metrics.most_recent_order_at,
         customer_metrics.average_delivery_time_from_collection,
-        customer_metrics.average_delivery_time_from_order
+        customer_metrics.average_delivery_time_from_order,
+        customer_survey.satisfaction_score,
+        customer_survey.survey_date,
+        {% for days in [30,90,360] %}
+        count_orders_last_{{days}}_days,
+        {%endfor%}
     from customers
     left join customer_metrics on (
         customers.customer_id = customer_metrics.customer_id
+    )
+    left join customer_survey on ( 
+        customers.email = customer_survey.customer_email
     )
 )
 
