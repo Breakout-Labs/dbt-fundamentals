229236e1607fcd4bdc64c5d78b86d0c6e333b3d8
Maria Ould Lamara <maria.ould-lamara@siemens.com>
2025-08-26 15:45:45 +0000

    Lab 2 completed

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -1,3 +1,4 @@
+{% set days = [30,90, 360]%}
 with orders as (
     select
         *
@@ -15,6 +16,12 @@ customers as (
     from {{ ref('stg_ecomm__customers') }}
 ),
 
+Customer_survey as (
+    select 
+    *
+    from {{ ref('stg_sheets__customer_survey_responses') }}
+),
+
 customer_metrics as (
     select
         customer_id,
@@ -22,7 +29,13 @@ customer_metrics as (
         min(ordered_at) as first_order_at,
         max(ordered_at) as most_recent_order_at,
         avg(delivery_time_from_collection) as average_delivery_time_from_collection,
-        avg(delivery_time_from_order) as average_delivery_time_from_order
+        avg(delivery_time_from_order) as average_delivery_time_from_order,
+        {% for days in [30,90,360] %}
+        count(case when ordered_at > current_date() - {{ days }} then 1 end) as count_orders_last_{{ days }}_days
+        {% if not loop.last %}
+        ,
+        {% endif %}
+        {% endfor %}
     from orders
     group by 1
 
@@ -32,13 +45,22 @@ joined as (
     select
         customers.*,
         coalesce(customer_metrics.count_orders,0) as count_orders,
+        customer_survey.satisfaction_score,  -- Add the satisfaction_score column
+        customer_survey.survey_date, 
         customer_metrics.first_order_at,
         customer_metrics.most_recent_order_at,
         customer_metrics.average_delivery_time_from_collection,
-        customer_metrics.average_delivery_time_from_order
+        customer_metrics.average_delivery_time_from_order,
+        {% for days in [30,90,360] %}
+        count_orders_last_{{ days }}_days,
+        {% endfor %}
     from customers
     left join customer_metrics on (
         customers.customer_id = customer_metrics.customer_id
+    )
+    left join Customer_survey on (
+        customers.email = customer_survey.customer_email
+
     )
 )
 
