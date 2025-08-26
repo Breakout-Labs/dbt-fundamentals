4b6feb4379c64f59f0f0fed73dc048e3c8def683
Pascal Brogl <pascal.brogl@siemens.com>
2025-08-26 11:24:44 +0000

    finalized lab 1

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -11,7 +11,7 @@ renamed as (
     select
         id as order_id,
         total_amount,
-        status as order_stats,
+        status as order_status,
         created_at as ordered_at,
         customer_id,
         store_id,
