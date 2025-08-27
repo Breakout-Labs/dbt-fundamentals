9365f02fa3d7e2d313be38c4ff1d4dd601d7e4cf
Timo Särkkä <timo@breakout-labs.com>
2025-08-27 13:33:50 +0000

    Used SQLFluff to Lint customers

--- models/marts/marketing/customers.sql
+++ models/marts/marketing/customers.sql
@@ -1,18 +1,15 @@
 with orders as (
-    select 
-        *
+    select *
     from {{ ref('orders') }}
-), 
+),
 
 customers as (
-    select 
-        *
+    select *
     from {{ ref('stg_ecomm__customers') }}
 ),
 
 survey_responses as (
-    select 
-        *
+    select *
     from {{ ref('stg_sheets__customer_survey_responses') }}
 ),
 
@@ -22,10 +19,12 @@ customer_metrics as (
         count(*) as count_orders,
         min(ordered_at) as first_order_at,
         max(ordered_at) as most_recent_order_at,
-        avg(delivery_time_from_collection) as average_delivery_time_from_collection,
+        avg(delivery_time_from_collection)
+            as average_delivery_time_from_collection,
         avg(delivery_time_from_order) as average_delivery_time_from_order,
         {% for days in [30, 90, 360] %}
-            count(case when ordered_at > current_date() - {{ days }} then 1 end) as count_orders_last_{{ days }}_days
+            count(case when ordered_at > current_date() - {{ days }} then 1 end)
+                as count_orders_last_{{ days }}_days
             {% if not loop.last %}
                 ,
             {% endif %}
@@ -38,7 +37,6 @@ customer_metrics as (
 joined as (
     select
         customers.*,
-        coalesce(customer_metrics.count_orders,0) as count_orders,
         customer_metrics.first_order_at,
         customer_metrics.most_recent_order_at,
         customer_metrics.average_delivery_time_from_collection,
@@ -47,16 +45,17 @@ joined as (
         customer_metrics.count_orders_last_90_days,
         customer_metrics.count_orders_last_360_days,
         survey_responses.survey_date,
-        survey_responses.satisfaction_score
+        survey_responses.satisfaction_score,
+        coalesce(customer_metrics.count_orders, 0) as count_orders
     from customers
-    left join customer_metrics on (
-        customers.customer_id = customer_metrics.customer_id
-    )
+    left join customer_metrics
+        on (
+            customers.customer_id = customer_metrics.customer_id
+        )
     left join survey_responses on (
         customers.email = survey_responses.customer_email
     )
 )
 
-select
-    *
+select *
 from joined
