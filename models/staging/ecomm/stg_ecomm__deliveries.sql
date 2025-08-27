74ecffc8ad74904e2c762e2b3725a92f29d1a089
Simo Tumelius <simo@datamie.fi>
2025-08-27 06:49:08 +0000

    Fix errors

--- models/staging/ecomm/stg_ecomm__deliveries.sql
+++ models/staging/ecomm/stg_ecomm__deliveries.sql
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-with source as (
-    select * from {{ source('ecomm', 'deliveries') }}
-),
-
-renamed as (
-    select
-        id as delivery_id,
-        order_id,
-        picked_up_at,
-        delivered_at,
-        status as delivery_status,
-        _synced_at
-    from source
-)
-
-select * from renamed
-=======
-{{ config(enabled=false) }}
+{{ config(enabled=true) }}
 
 with source as (
     select * from {{ source('ecomm', 'deliveries') }}
@@ -34,4 +16,3 @@ renamed as (
 )
 
 select * from renamed
->>>>>>> 6cc19d6e822eca6ec3317478cea686b15966eaa0
