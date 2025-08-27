b18fa6121f7ed87827f6d1be6565cea9780ab25b
Kevin Campbell <kevin-campbell@siemens.com>
2025-08-27 13:30:04 +0000

    lab 4 completed

--- models/intermediate/marketing/int_marketing__customer_order_stats.sql
+++ models/intermediate/marketing/int_marketing__customer_order_stats.sql
@@ -1,7 +1,7 @@
-select 
+select
     customer_id,
     round(avg(total_amount), 2) as avg_order_amount,
     count(*) as order_count
-    from {{ ref("orders") }}
-    where ordered_at > current_date - 180
-    group by 1
+from {{ ref("orders") }}
+where ordered_at > current_date - 180
+group by 1
