9bf930a32f29df48df4d897f34d874d2f76f4781
Konstantin Schmidt <konstantin.schmidt@siemens.com>
2025-08-27 10:25:01 +0000

    Lab 3 completed!

---
+++ models/marts/marketing/seven_week_active_customers.sql
@@ -0,0 +1,35 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with
+    customers as (select * from {{ ref("customers") }}),
+
+    active_customers as (
+        select 
+        *
+        from {{ ref("int_marketing__seven_week_active_customers") }}
+    ),
+
+    metrics as (
+        select
+        *
+        from {{ ref('int_marketing__customer_order_stats') }}
+    ),
+
+    joined as (
+        select
+            metrics.customer_id,
+            customers.first_name,
+            customers.last_name,
+            metrics.avg_order_amount,
+            metrics.order_count
+        from customers
+        inner join metrics using (customer_id)
+        inner join active_customers using (customer_id)
+    )
+
+select *
+from joined
