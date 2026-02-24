415a4a1c91a79a83297c9cc20fc5930a80337542
lukrecia maljarova <lukrecia.maljarova@siemens.com>
2026-02-24 13:34:32 +0000

    Lab 1 completed!

---
+++ models/marts/orders.sql
@@ -0,0 +1,25 @@
+with orders as (
+select *
+from {{ref('stg_ecomm__orders')}}
+),
+
+deliveries as (
+  select *
+  from {{ref('stg_ecomm__deliveries')}}
+),
+deliveries_filtered as (
+  select *
+  from deliveries
+  where delivery_status like 'delivered'
+),
+
+joined as (
+select orders.order_id, customer_id, ordered_at, order_status, total_amount, store_id,
+datediff('minute', orders.ordered_at, deliveries_filtered.delivered_at) as delivery_time_from_order,
+datediff('minute', deliveries_filtered.picked_up_at, deliveries_filtered.delivered_at) as delivery_time_from_collection
+from orders
+left join deliveries_filtered on (orders.order_id = deliveries_filtered.order_id)
+)
+select
+  *
+from joined
