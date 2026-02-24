011883b2a11ddf8a8241c775924ff9df2caf8cc8
Tobias Wild <tobias.wild@siemens.com>
2026-02-24 12:02:03 +0000

    Lab 1 completed!

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -1,10 +1,11 @@
+
 with orders as (
-    select
-        id as order_id,
-        customer_id,
-        created_at as ordered_at
-    from raw.ecomm.orders_us
-), 
+  select
+      *
+  from {{ ref('orders') }} 
+),
+
+
 
 customers as (
     select
@@ -17,11 +18,15 @@ customer_metrics as (
         customer_id,
         count(*) as count_orders,
         min(ordered_at) as first_order_at,
-        max(ordered_at) as most_recent_order_at
+        max(ordered_at) as most_recent_order_at,
+        avg(delivery_time_from_collection) as average_delivery_time_from_collection,
+        avg(delivery_time_from_order) as average_delivery_time_from_order
     from orders
     group by 1
 
 ),
+
+
 
 joined as (
     select
@@ -29,6 +34,7 @@ joined as (
         coalesce(customer_metrics.count_orders,0) as count_orders,
         customer_metrics.first_order_at,
         customer_metrics.most_recent_order_at
+
     from customers
     left join customer_metrics on (
         customers.customer_id = customer_metrics.customer_id
