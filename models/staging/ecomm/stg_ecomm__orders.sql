a4615a163023c4e9bd3301b105fac09617311476
Gabriele Albrecht <gabriele.albrecht@siemens.com>
2026-02-24 12:22:51 +0000

    Lab 1 completed!

---
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -0,0 +1,25 @@
+With source as
+(
+    select 
+    *
+    from {{ source('ecomm', 'orders') }} 
+),
+
+renamed as 
+(
+    select
+    id as order_id,
+    created_at as ordered_at,
+    status as order_status,
+    *
+    from source
+),
+
+final as
+(
+    select
+    * from renamed
+)
+select
+ *
+from final
