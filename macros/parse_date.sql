e867cfb9650370aa2f0504fe154f3fb39b27d1fc
Ping-Yu Huang <ping-yu.huang@siemens.com>
2026-02-25 12:13:32 +0000

    Lab 3 completed

--- macros/parse_date.sql
+++ macros/parse_date.sql
@@ -3,9 +3,9 @@
 {% macro parse_date(survey_date) -%}
 
 coalesce(
-    try_to_date({{survey_date}}, 'MM/DD/YY'),    
-    try_to_date({{survey_date}}, 'MM/DD/YYYY'),
-    try_to_date({{survey_date}}, 'YYYY-MM-DD')
+    try_to_date({{ survey_date }}, 'MM/DD/YY'),    
+    try_to_date({{ survey_date }}, 'MM/DD/YYYY'),
+    try_to_date({{ survey_date }}, 'YYYY-MM-DD')
 )
 
 {% endmacro %}
