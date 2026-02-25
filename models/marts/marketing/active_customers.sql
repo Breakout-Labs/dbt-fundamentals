1a7619c0563204a5f4899c993560b4d3512e42c7
Michael Kreussel <michael.kreussel@siemens.com>
2026-02-25 11:59:40 +0000

    Completed Lab 3

---
+++ models/marts/marketing/active_customers.sql
@@ -0,0 +1,31 @@
+with customers as (
+    select
+        *
+    from {{ ref('customers') }}
+),
+
+customer_order_stats as (
+    select
+    *
+    from {{ ref('int_marketing__customer_order_stats') }}
+
+),
+
+joined as (
+    select
+        customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_order_stats.orders_ordered,
+        customer_order_stats.orders_pending,
+        customer_order_stats.orders_cancelled,
+        customer_order_stats.orders_delivered,
+        customer_order_stats.avg_order_amount,
+        customer_order_stats.orders_total
+    from customers
+    inner join customer_order_stats using (customer_id)
+)
+
+select
+    *
+from joined
