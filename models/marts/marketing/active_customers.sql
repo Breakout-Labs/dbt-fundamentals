1023559441c5c2f151d153e6980b36abd4a5c8c0
Michael Kreussel <michael.kreussel@siemens.com>
2026-02-25 12:18:34 +0000

    refinement of sql

--- models/marts/marketing/active_customers.sql
+++ models/marts/marketing/active_customers.sql
@@ -16,12 +16,13 @@ joined as (
         customer_id,
         customers.first_name,
         customers.last_name,
-        customer_order_stats.orders_ordered,
-        customer_order_stats.orders_pending,
-        customer_order_stats.orders_cancelled,
-        customer_order_stats.orders_delivered,
-        customer_order_stats.avg_order_amount,
-        customer_order_stats.orders_total
+        customer_order_stats.* exclude (customer_id)
+        --customer_order_stats.orders_ordered,
+        --customer_order_stats.orders_pending,
+        --customer_order_stats.orders_cancelled,
+        --customer_order_stats.orders_delivered,
+        --customer_order_stats.avg_order_amount,
+        --customer_order_stats.orders_total
     from customers
     inner join customer_order_stats using (customer_id)
 )
