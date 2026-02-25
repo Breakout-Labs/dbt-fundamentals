57b4ebfd8b168b35d200f02abeebd10696f1ea84
Tobias Wild <tobias.wild@siemens.com>
2026-02-25 09:49:01 +0000

    Lab 2 completed!

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -5,7 +5,11 @@ with orders as (
   from {{ ref('orders') }} 
 ),
 
-
+customer_survey as (
+    select 
+    *
+    from {{ ref('stg_sheets__customer_survey_responses') }}
+),
 
 customers as (
     select
@@ -20,7 +24,12 @@ customer_metrics as (
         min(ordered_at) as first_order_at,
         max(ordered_at) as most_recent_order_at,
         avg(delivery_time_from_collection) as average_delivery_time_from_collection,
-        avg(delivery_time_from_order) as average_delivery_time_from_order
+        avg(delivery_time_from_order) as average_delivery_time_from_order,
+        {%- for days in [30,90, 360] %}
+        count_if(ordered_at > current_date() - {{days}}) as count_orders_last_{{days}}_days
+        {%- if not loop.last %} , {% endif -%} 
+        {%- endfor %}
+
     from orders
     group by 1
 
@@ -33,11 +42,19 @@ joined as (
         customers.*,
         coalesce(customer_metrics.count_orders,0) as count_orders,
         customer_metrics.first_order_at,
-        customer_metrics.most_recent_order_at
+        customer_metrics.most_recent_order_at,
+        customer_metrics.average_delivery_time_from_collection,
+        customer_metrics.average_delivery_time_from_order,
+        customer_metrics.count_orders_last_30_days,
+        customer_metrics.count_orders_last_90_days,
+        customer_metrics.count_orders_last_360_days
 
     from customers
     left join customer_metrics on (
         customers.customer_id = customer_metrics.customer_id
+    )
+    left join customer_survey  on (
+        customers.email = customer_survey.customer_email
     )
 )
 
