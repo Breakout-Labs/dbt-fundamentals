4874d58b126f5bef6e159c823b01e7e7b2040016
Hana Taylor <rahana.taylor@siemens.com>
2026-05-26 17:40:22 +0000

    Lab 1 completed

--- models/staging/ecomm/stg_ecomm__customers.sql
+++ models/staging/ecomm/stg_ecomm__customers.sql
@@ -1,22 +1,24 @@
-with source as (
-  select
-    *
-  from {{ source('ecomm', 'customers') }}
+with 
+
+source as (
+
+    select * from {{ source('ecomm', 'customers') }}
+
 ),
 
 renamed as (
-  select
-    id as customer_id,
-    *
-  from source
-),
 
-final as (
-  select
-    *
-  from renamed
+    select
+        id,
+        total_amount,
+        status,
+        created_at,
+        customer_id,
+        store_id,
+        _synced_at
+
+    from source
+
 )
 
-select
-  *
-from final
+select * from renamed
