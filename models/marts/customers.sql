dd869a4b56caa8ac7e621deb60cba2fded61a80e
Oksana Koltsa <oksana.koltsa.ext@siemens.com>
2026-05-27 15:54:53 +0000

    Lab 2 . Part 1. Added columns based on days using Jinja

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -1,3 +1,4 @@
+{% set days = [30,90,360] %}
 with orders as (
     select *
         /*order_id,
@@ -16,6 +17,10 @@ customer_metrics as (
     select
         customer_id,
         count(*) as count_orders,
+        {% for day in days %}
+        count_if(ordered_at > current_date() - {{ day }} ) as count_orders_last_{{ day }}_days
+        {% if not loop.last %},{% endif %}
+        {% endfor %},
         min(ordered_at) as first_order_at,
         max(ordered_at) as most_recent_order_at
     from orders
@@ -28,7 +33,10 @@ joined as (
         customers.*,
         coalesce(customer_metrics.count_orders,0) as count_orders,
         customer_metrics.first_order_at,
-        customer_metrics.most_recent_order_at
+        customer_metrics.most_recent_order_at,
+        {% for day in days %}
+        customer_metrics.count_orders_last_{{ day }}_days{% if not loop.last %},{% endif %}
+        {% endfor %}
     from customers
     left join customer_metrics on (
         customers.customer_id = customer_metrics.customer_id
