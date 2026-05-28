b191f24596976d7483b0374bb49cd9bb9017cf95
Alexander Seger <alexander.seger@siemens.com>
2026-05-28 14:41:17 +0000

    Finish Lab #3

---
+++ models/intermediate/int_marketing__customer_order_stats.sql
@@ -0,0 +1,7 @@
+select
+          customer_id,                                                                                                                                                                                                                                                                                                 
+          count(*) as total_orders,
+          count(case when order_status = 'delivered' then 1 end) as orders_delivered,
+          round(avg(total_amount), 2) as avg_order_amount                                                                                                                                                                                                                                                              
+      from {{ ref('orders') }}
+      group by customer_id
