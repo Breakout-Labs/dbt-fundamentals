9e4bd969c9ea38847baf97458fbc3a9e42aae4d6
Ana Veiga <ana-rita.veiga@siemens.com>
2026-05-28 13:35:51 +0000

    Lab 2 completed!

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -1,8 +1,12 @@
+
+{% set days=[30,90,360] %}
 with orders as (
         select
         *
         from {{ ref('stg_ecomm__orders') }}
     ),
+
+customer_survey_responses as (select * from {{ref("stg_sheets__customer_survey_responses")}}),
 
 customers as (
     select
@@ -14,7 +18,11 @@ customer_metrics as (
             customer_id,
             count(*) as count_orders,
             min(ordered_at) as first_order_at,
+              {% for day in days %}
+                count_if(ordered_at > current_date() - {{day}}) as count_orders_last_{{day}}_days,
+            {% endfor %}
             max(ordered_at) as most_recent_order_at
+          
         from orders
         group by 1
     ),
@@ -24,11 +32,19 @@ joined as (
         customers.*,
         coalesce(customer_metrics.count_orders,0) as count_orders,
         customer_metrics.first_order_at,
-        customer_metrics.most_recent_order_at
+        customer_metrics.most_recent_order_at,
+        customer_survey_responses.satisfaction_score,
+        customer_survey_responses.survey_date,        
+        customer_metrics.count_orders_last_30_days,     
+        customer_metrics.count_orders_last_90_days,
+        customer_metrics.count_orders_last_360_days            
     from customers
     left join customer_metrics on (
         customers.customer_id = customer_metrics.customer_id
-    )
+         )
+    left join customer_survey_responses on (
+        customers.email=customer_survey_responses.customer_email
+        )
 )
 
 select
